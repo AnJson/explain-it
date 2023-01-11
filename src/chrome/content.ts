dac9e96b50b7410746462c0bc6194c2f34342c65
Anders Jonsson <aj224rj@student.lnu.se>
2023-01-11 21:42:21 +0100

    Add initial code to test the selectionchange handler

--- src/chrome/content.ts
+++ src/chrome/content.ts
@@ -1,1 +1,12 @@
-console.log('content script is here!')
+// Listen for selectionchange event
+document.addEventListener('selectionchange', (event) => {
+  console.log(event)
+  // Get the selected text
+  const selectedText = window.getSelection()?.toString()
+
+  // Send a message to the background script with the selected text
+  chrome.runtime.sendMessage({
+    type: 'selectionchange',
+    selectedText
+  })
+})
