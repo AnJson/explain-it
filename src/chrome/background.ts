8452304539412a98316792fdd2321906968c1b75
Anders Jonsson <aj224rj@student.lnu.se>
2023-01-13 22:03:12 +0100

    Possibly solve the eventlistener end message sending

--- src/chrome/background.ts
+++ src/chrome/background.ts
@@ -1,3 +1,17 @@
-chrome.tabs.onActivated.addListener((activeInfo) => {
+/* chrome.tabs.onActivated.addListener((activeInfo) => {
   chrome.tabs.executeScript(activeInfo.tabId, { file: 'content.js' })
+}) */
+
+/* chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+  if (changeInfo.status === 'complete') {
+    chrome.tabs.executeScript(tabId, {
+      file: 'content.js'
+    })
+  }
+}) */
+
+chrome.tabs.onUpdated.addListener((tabId) => {
+  chrome.tabs.executeScript(tabId, {
+    file: 'content.js'
+  })
 })
