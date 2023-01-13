971982cd960ba8a38ea249cc5704b9fab99bca2f
Anders Jonsson <aj224rj@student.lnu.se>
2023-01-14 00:01:09 +0100

    Fix some errors regarding message sending and event listening active tab

--- src/chrome/background.ts
+++ src/chrome/background.ts
@@ -10,8 +10,22 @@
   }
 }) */
 
-chrome.tabs.onUpdated.addListener((tabId) => {
-  chrome.tabs.executeScript(tabId, {
-    file: 'content.js'
-  })
+function injectContentScript(status: string | undefined, tabId: number) {
+  if (status === 'complete') {
+    chrome.scripting.executeScript({
+      target: {
+        tabId,
+        allFrames: true
+      },
+      files: ['./static/js/content.js']
+    })
+  } else {
+    setTimeout(() => {
+      injectContentScript(status, tabId)
+    }, 200)
+  }
+}
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+  injectContentScript(changeInfo.status, tabId)
 })
